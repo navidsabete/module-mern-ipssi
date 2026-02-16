f44a272e8bf8e98c1b47be61615ff28d849c503d
Navid SABETE <nevid.dev@gmail.com>
2026-02-16 17:43:11 +0100

    TP 'mon premier react' setup

---
+++ tp-mon-premier-react/src/App.tsx
@@ -0,0 +1,12 @@
+function App() { 
+  const prenom = "Navid";
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}> 
+        <h1>🌍 Hello {prenom} !</h1> 
+        <p>Mon premier composant React fonctionne très bien !</p> 
+        </div> 
+    ); 
+} 
+
+
+export default App;
