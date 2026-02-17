172719d8d10a7a592dac782977960cf5a74e209e
Navid SABETE <nevid.dev@gmail.com>
2026-02-17 09:50:19 +0100

    compteur interactif - useState

--- tp-mon-premier-react/src/App.tsx
+++ tp-mon-premier-react/src/App.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+
 function App() { 
-  const prenom = "Navid";
+  const [count, setCount] = useState(0);
+  const increment = () => { 
+    setCount(count+1);
+  };
+  const decrement = () => { 
+    setCount(count-1);
+  };
+  
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}> 
-        <h1>🌍 Hello {prenom} !</h1> 
-        <p>Mon premier composant React fonctionne très bien !</p> 
-        </div> 
+        <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}> 
+        <h1>Compteur Interactif</h1> 
+        <p style={{ fontSize: '40px', fontWeight: 'bold', color: count<0?'red':'black' }}> 
+          {count} 
+        </p> 
+        <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
+          <button onClick={decrement} style={{ padding: '10px 20px' }}> 
+            - Diminuer 
+          </button> 
+          <button onClick={increment} style={{ padding: '10px 20px' }}> 
+            + Augmenter 
+          </button> 
+        </div>
+      </div>
+
+
     ); 
 } 
 
