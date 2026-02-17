cf497b9a5d8bcfc989db6971b8701a974c9e1a2d
Navid SABETE <nevid.dev@gmail.com>
2026-02-17 12:16:10 +0100

    composants et props

--- tp-mon-premier-react/src/App.tsx
+++ tp-mon-premier-react/src/App.tsx
@@ -1,18 +1,48 @@
-import { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Routes, Route, Link} from "react-router-dom";
+import BookCard from "./components/BookCard";
 
+  const myBooks = [ 
+  { id: 1, title: "Mon Livre 1", author: "Mon Auteur 1" },
+  { id: 2, title: "Mon Livre 2", author: "Mon Auteur 2" }, 
+  { id: 3, title: "Mon Livre 3", author: "Mon Auteur 3" } ];
 
-const Home = () => <h2>Page d'accueil</h2>; 
-const Library = () => <h2>Ma Bibliothèque</h2>;
-const Compteur = () => {
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
+      <h2>Page d'accueil</h2>
+    </div>
+  );
+} 
+function Library() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}> 
+      <h2>Ma Bibliothèque</h2>
+      <div className="book-list">
+        {myBooks.map((book) => (
+          <BookCard
+          key={book.id} title={book.title} author={book.author}  
+          />
+          ))}
+      </div>
+    </div>
+  );
+} 
+function Compteur() {
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => {
+    const savedValue = localStorage.getItem("compteurCount");
+    return savedValue !== null ? Number(savedValue) : 0;
+  });
   const increment = () => { 
     setCount(count+1);
   };
   const decrement = () => { 
     setCount(count-1);
   };
+  useEffect(() => {
+    localStorage.setItem("compteurCount", count.toString());
+  }, [count]);
 return(
 <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}> 
  <h2>Mon Compteur Interactif</h2>
@@ -20,10 +50,10 @@ return(
           {count} 
         </p> 
         <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
-          <button onClick={decrement} style={{ padding: '10px 20px', backgroundColor: 'lightyellow' }}> 
+          <button onClick={decrement} style={{ padding: '10px 20px' }}> 
             - Diminuer 
           </button> 
-          <button onClick={increment} style={{ padding: '10px 20px', backgroundColor: 'lightblue' }}> 
+          <button onClick={increment} style={{ padding: '10px 20px'}}> 
             + Augmenter 
           </button> 
         </div>
@@ -33,7 +63,7 @@ return(
 
  const ErrorNotFound = () => {
   return(
-    <div>
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}> 
       <h2>Erreur 404 : Page introuvable</h2><Link to="/">⬅ Retour à l'accueil</Link>
     </div>
   );
@@ -43,7 +73,7 @@ function App() {
   const [countClick, setCountClick] = useState(0);
     return (
         <div> 
-          <nav style={{ padding: '20px', background: '#eee' }}>
+          <nav style={{ background: '#eee' }}>
             <Link to="/" onClick={() => setCountClick(countClick + 1)}>Accueil</Link> | <Link to="/library" onClick={() => setCountClick(countClick + 1)}>Livres</Link> | <Link to="/compteur" onClick={() => setCountClick(countClick + 1)}>Compteur</Link> | <span>Compteur clic : {countClick}</span>
             </nav> 
             <main style={{ padding: '20px' }}> 
